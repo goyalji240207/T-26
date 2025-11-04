d74f20cdcedd777c8471b1c76dbe53d775d4a723
Vibhor Srivastava <153298754+itsmevibhor2005@users.noreply.github.com>
2025-11-04 22:05:13 +0530

    Revert "changes"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GoogleLogin from './GoogleLogin.jsx'
-
 
 function App() {
   
 
   return (
     <>
-      <h1 className='font-bold text-5xl my-2'>Techkriti' 26</h1>
+    <h1 className='font-bold text-5xl my-2'>Techkriti' 26</h1>
       <button className="bg-blue-500 text-white px-4 py-2 rounded hover:cursor-pointer">Login</button>
-      <GoogleLogin />
     </>
   )
 }
